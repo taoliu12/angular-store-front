b38de1f7e3a56f301b74a93b53440cbfcb3ca933
taoliu12 <werewaswas@live.com>
2019-06-20 11:24:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-r9rr74
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-r9rr74)
---
+++ src/app/last-viewed/last-viewed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-last-viewed',
+  templateUrl: './last-viewed.component.html',
+  styleUrls: ['./last-viewed.component.css']
+})
+export class LastViewedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
